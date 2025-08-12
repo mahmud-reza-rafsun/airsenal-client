fbe73cd77630abca8f11fa1779ae66b1a2858b35
Rafsun <rafsun@rafsun.dev>
2025-08-13 00:38:54 +0600

    add all product with tag search

--- src/Pages/AllProducts/AllProductCard.jsx
+++ src/Pages/AllProducts/AllProductCard.jsx
@@ -1,23 +1,25 @@
 const AllProductCard = ({ product }) => {
     const { _id, productName, newTag, description, image, votes } = product || {};
     return (
-        <div className="border p-3 rounded-md border-gray-400">
+        <div className="border p-3 rounded-md border-gray-300">
             <figure>
                 <img
                     src={image}
                     className="rounded-md h-48 object-cover w-full"
                     alt="Shoes" />
             </figure>
-            <div className="my-5 space-y-3">
+            <div className="mt-3 space-y-3">
                 <h2 className="text-lg lg:text-xl font-semibold">{productName}</h2>
-                <p className="text-gray-600 text-[15px]">{description.substring(0, 100)}</p>
+                <p className="text-gray-600 text-[15px]">{description.substring(0, 100)}.....</p>
 
-                <div className="">
-                    {newTag?.slice(0, 3).map((tag, index) => (
-                        <span key={index} className="w-full px-3 py-1 mr-1 cursor-pointer bg-blue-100 text-blue-500 rounded-full text-[13px] text-center">
-                            #{tag}
-                        </span>
-                    ))}
+                {newTag?.slice(0, 3).map((tag, index) => (
+                    <span key={index} className="w-full px-3 py-1 mr-1 cursor-pointer bg-blue-100 text-blue-500 rounded-full text-[13px] text-center">
+                        #{tag}
+                    </span>
+                ))}
+                <div className="flex justify-between items-center mt-4">
+                    <button className="btn btn-sm rounded-xl bg-indigo-400 text-white">DownVote: 0</button>
+                    <button className="btn btn-sm rounded-xl bg-indigo-400 text-white">UpVote: {votes}</button>
                 </div>
             </div>
         </div>
--- src/Pages/AllProducts/AllProducts.jsx
+++ src/Pages/AllProducts/AllProducts.jsx
@@ -1,23 +1,45 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 import AllProductCard from './AllProductCard';
+import { useState } from 'react';
 
 const AllProducts = () => {
-    const { data: products = [], isLoading } = useQuery({
-        queryKey: ['products'],
+    const [search, setSearch] = useState('');
+    const { data: products = [] } = useQuery({
+        queryKey: ['products', search],
         queryFn: async () => {
-            const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/get-product`)
+            const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/all-products?search=${search}`)
             return data;
         }
     })
+
     console.log(products);
-    if (isLoading) return <LoadingSpinner />
+
     return (
         <div>
             <div className='text-center'>
-                <h2 className='text-xl lg:text-3xl font-semibold'>Discover Our <span className='text-indigo-500'>Products</span> </h2>
+                <h2 className='text-xl lg:text-3xl mt-5 font-semibold'>Discover Our <span className='text-indigo-500'>Products</span> </h2>
                 <p className='text-gray-500 w-10/12 mx-auto pt-4'>Explore our handpicked selection of top-quality products that stand out for their exceptional value, unique design, and customer favorites. Each featured item is carefully chosen to meet high standards of quality, functionality, and style—ensuring you get only the best.</p>
+            </div>
+            <div className="mt-8">
+                <div className='flex flex-col md:flex-row justify-center items-center gap-5'>
+
+                    <div className='flex p-1 overflow-hidden border rounded-lg focus-within:ring focus-within:ring-opacity-30 focus-within:border-gray-400 focus-within:ring-gray-400'>
+                        <input
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-base-100 outline-none focus:placeholder-transparent'
+                            type='text'
+                            name='search'
+                            placeholder='Enter The Tag'
+                            aria-label='Enter Job Title'
+                        />
+
+                        <button className='px-3 md:hidden lg:items-center lg:flex md:px-4 py-2 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-indigo-500 rounded-md hover:bg-indigo-600 focus:bg-indigo-600 cursor-pointer focus:outline-none'>
+                            Search
+                        </button>
+                    </div>
+                </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-12">
                 {
